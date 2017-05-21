ccdc792e348430edff09fea4a1399dcb0e017082
Matthew Klein <mklein994@gmail.com>
2017-05-20 23:18:08 -0500

    Start in memory web api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,14 @@ import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 import { AppComponent } from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
+import { DashboardComponent } from './dashboard.component';
 import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService } from './hero.service';
-import { DashboardComponent } from './dashboard.component'
 
 @NgModule({
   declarations: [
@@ -22,12 +25,11 @@ import { DashboardComponent } from './dashboard.component'
     BrowserModule,
     FormsModule,
     HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
-  providers: [
-    HeroService
-  ],
-  bootstrap: [AppComponent]
+  providers: [ HeroService ],
+  bootstrap: [ AppComponent ]
 })
 
 
