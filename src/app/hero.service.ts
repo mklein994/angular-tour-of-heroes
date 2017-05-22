7a90abf216616823380fec03a77682706d4445ec
Matthew Klein <mklein994@gmail.com>
2017-05-22 12:38:28 -0500

    CRUD: implement create/add

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -19,6 +19,14 @@ export class HeroService {
                .catch(this.handleError);
   }
 
+  create(name: string): Promise<Hero> {
+    return this.http
+      .post(this.heroesUrl, JSON.stringify({name: name}), {headers: this.headers})
+      .toPromise()
+      .then(res => res.json().data as Hero)
+      .catch(this.handleError);
+  }
+
   update(hero: Hero): Promise<Hero> {
     const url = `${this.heroesUrl}/${hero.id}`;
     return this.http
